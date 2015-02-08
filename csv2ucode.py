fba009fb8fac8125a604b99f4507761c5d11c62a
nykh <nicholas.ykhuang@gmail.com>
2015-02-08 00:48:28 -0600

    update csv2ucode.py to make it easier to modify

--- csv2ucode.py
+++ csv2ucode.py
@@ -1,6 +1,11 @@
 import sys
 import csv
 
+# Edit this variable to set the end column of the .csv file.
+# Note: In Python convention it must be ONE BEYOND the last column, zero-based
+# for example, the column runs from column 1 to column 23, then column_end must be 24
+column_end = 24
+
 if len(sys.argv) != 3:
 	print('usage: ', __file__, ' <csv file> <ucode file>')
 	sys.exit(1)
@@ -10,7 +15,7 @@ with open(sys.argv[1]) as csvfile, open(sys.argv[2], 'w') as ucode:
 	
 	next(cr) # ignore first line
 	for row in cr:
-		for x in row[1:24]:
+		for x in row[1:column_end]:
 			ucode.write(x.strip())
 		ucode.write('\n')
 			
